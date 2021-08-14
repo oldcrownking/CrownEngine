7ccda70d6c8245e239c4523cd40f7154fd22f3a5
EpicCrownKing <52501627+EpicCrownKing@users.noreply.github.com>
2021-08-14 13:19:33 -0700

    wip water

--- CenturionGame/Content/Sandbox.cs
+++ CenturionGame/Content/Sandbox.cs
@@ -7,6 +7,7 @@ using Microsoft.Xna.Framework.Content;
 using CrownEngine;
 using System.Diagnostics;
 using CrownEngine.Prefabs;
+using Microsoft.Xna.Framework.Input;
 
 namespace CenturionGame.Content
 {
@@ -21,66 +22,54 @@ namespace CenturionGame.Content
 
         public override void Load()
         {
+            /*AddActor(new NonsolidTilemap(Vector2.Zero, this, new int[,] {
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 0, 0, 0, 0, 2, 2, 0, 0, 0, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 0, 0, 0, 0, 2, 2, 0, 0, 0, 2, 2, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 2, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 2, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 1, 1, 0, 0, 2, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 1, 1, 0, 2, 2, 0, 1, 0, 2, 2, 2, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 1, 1, 0, 2, 2, 0, 1, 0, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                }, new List<Texture2D>() { EngineHelpers.GetTexture("Dirt2"), EngineHelpers.GetTexture("Stone2") }, 8));*/
+
             AddActor(new SolidTilemap(Vector2.Zero, this, new int[,] {
-                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
-                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
-                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
-                { 1, 1, 0, 0, 0, 0, 3, 3, 0, 0, 0, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1, 1, 1 },
-                { 1, 1, 0, 0, 0, 0, 3, 3, 0, 0, 0, 2, 2, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1 },
-                { 1, 1, 0, 0, 0, 0, 3, 3, 0, 0, 0, 0, 2, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1 },
-                { 1, 1, 0, 0, 0, 0, 3, 3, 0, 0, 0, 0, 2, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1 },
-                { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1 },
-                { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1 },
-                { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1 },
-                { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1 },
-                { 1, 1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 1, 1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 1, 1, 0, 2, 2, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 1, 1, 0, 2, 2, 0, 0, 0, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
-                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
-                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
-                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+                { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+                { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+                { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+                { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+                { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+                { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,},
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,},
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,},
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,},
                 }, new List<Texture2D>() { EngineHelpers.GetTexture("Grass"), EngineHelpers.GetTexture("Dirt"), EngineHelpers.GetTexture("Stone") }, 8));
 
-            AddActor(new Player(new Vector2(64, 64), this));
+            AddActor(new Player(new Vector2(96, 32), this));
+
+            AddActor(new Water(new Vector2(16, 68), this, 72, 44));
         }
 
-        public int sceneTransTimer = -1;
-        public bool sceneTranssing = false;
-        public override void Draw(SpriteBatch spriteBatch)
-        {
-            base.Draw(spriteBatch);
-
-            if (sceneTransTimer % 6 == 0)
-            {
-                for (int i = 0; i < 8; i++)
-                {
-                    squares.Add(new sceneTransSquare(new Vector2(4 + (i * 8), 4 + ((sceneTransTimer * 8) / 6)), 0f));
-                }
-            }
-
-            for (int j = 0; j < squares.Count; j++)
-            {
-                var square = squares[j];
-                square.Update();
-                squares[j] = square;
-
-                spriteBatch.Draw(EngineHelpers.GetTexture("MagicPixel"), squares[j].pos, new Rectangle(0, 0, 2, 2), Color.Black, 0f, new Vector2(1, 1), squares[j].size, SpriteEffects.None, 0f);
-            }
-
-            if (sceneTranssing) sceneTransTimer++;
-
-            if (sceneTransTimer > 80) EngineHelpers.SwitchStages(0);
-
-            VertexPositionColor[] vertices = new VertexPositionColor[3];
-
-            vertices[0] = new VertexPositionColor(new Vector3(-0.5f, -0.5f, 0), Color.Blue);
-            vertices[1] = new VertexPositionColor(new Vector3(0, -0.5f, 0), Color.Blue);
-            vertices[2] = new VertexPositionColor(new Vector3(-0.5f, 0, 0), Color.Blue);
-            EngineGame.instance.GraphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleList, vertices, 0, 1);
-        }
-
-        public List<sceneTransSquare> squares = new List<sceneTransSquare>();
+        /*public List<sceneTransSquare> squares = new List<sceneTransSquare>();
 
         public struct sceneTransSquare
         {
@@ -99,6 +88,34 @@ namespace CenturionGame.Content
 
                 size = size.Clamp(0, 4f);
             }
+        }*/
+
+        //public int sceneTransTimer = -1;
+        //public bool sceneTranssing = false;
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+
+            /*if (sceneTransTimer % 6 == 0)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    squares.Add(new sceneTransSquare(new Vector2(4 + (i * 8), 4 + ((sceneTransTimer * 8) / 6)), 0f));
+                }
+            }
+
+            for (int j = 0; j < squares.Count; j++)
+            {
+                var square = squares[j];
+                square.Update();
+                squares[j] = square;
+
+                spriteBatch.Draw(EngineHelpers.GetTexture("MagicPixel"), squares[j].pos, new Rectangle(0, 0, 2, 2), Color.Black, 0f, new Vector2(1, 1), squares[j].size, SpriteEffects.None, 0f);
+            }
+
+            if (sceneTranssing) sceneTransTimer++;
+
+            if (sceneTransTimer > 80) EngineHelpers.SwitchStages(0);*/
         }
     }
 }
--- CenturionGame/Content/Water.cs
+++ CenturionGame/Content/Water.cs
@@ -7,81 +7,196 @@ using Microsoft.Xna.Framework.Content;
 using CrownEngine;
 using System.Diagnostics;
 using CenturionGame;
+using Microsoft.Xna.Framework.Input;
 
 namespace CenturionGame.Content
 {
     public class Water : Actor
     {
-        public Water(Vector2 pos, Stage myStage) : base(pos, myStage)
+        public int depth;
+        public int waterWidth;
+
+        public Water(Vector2 pos, Stage myStage, int _width, int _depth) : base(pos, myStage)
         {
-            
+            waterWidth = _width;
+            depth = _depth;
         }
 
         public override void Load()
         {
+            effect = new BasicEffect(EngineGame.instance.GraphicsDevice)
+            {
+                VertexColorEnabled = true
+            };
+
+            for(int i = 0; i < (waterWidth / 4) + 1; i++)
+            {
+                waterParticles.Add(new WaterParticle((i * 4) + (int)position.X, (int)position.Y));
+            }
+
             base.Load();
         }
 
-        public float Position, Velocity;
-        public float Height;
         public override void Update()
         {
-            /*const float k = 0.025f; // adjust this value to your liking
-            float x = Height - 64;
-            float acceleration = -k * x;
-
-            Position += Velocity;
-            Velocity += acceleration;
-
-            for (int i = 0; i < springs.Length; i++)
-                springs[i].Update(Dampening, Tension);
-
-            float[] leftDeltas = new float[springs.Length];
-            float[] rightDeltas = new float[springs.Length];
-
-            // do some passes where springs pull on their neighbours
-            for (int j = 0; j < 8; j++)
+            if (EngineGame.instance.mouseState.LeftButton == ButtonState.Pressed && EngineGame.instance.oldMouseState.LeftButton == ButtonState.Released)
             {
-                for (int i = 0; i < springs.Length; i++)
-                {
-                    if (i > 0)
-                    {
-                        leftDeltas[i] = Spread * (springs[i].Height - springs[i - 1].Height);
-                        springs[i - 1].Speed += leftDeltas[i];
-                    }
-                    if (i < springs.Length - 1)
-                    {
-                        rightDeltas[i] = Spread * (springs[i].Height - springs[i + 1].Height);
-                        springs[i + 1].Speed += rightDeltas[i];
-                    }
-                }
+                Splash((EngineGame.instance.mouseState.Position.X / EngineGame.instance.windowScale), 10f);
+            }
 
-                for (int i = 0; i < springs.Length; i++)
-                {
-                    if (i > 0)
-                        springs[i - 1].Height += leftDeltas[i];
-                    if (i < springs.Length - 1)
-                        springs[i + 1].Height += rightDeltas[i];
-                }
-
-                base.Update();
-            }*/
+            base.Update();
         }
 
-        /*public void Splash(int index, float speed)
+        public List<WaterParticle> waterParticles = new List<WaterParticle>();
+
+        public struct WaterParticle
         {
-            if (index >= 0 && index < springs.Length)
-                springs[i].Speed = speed;
-        }*/
+            public int x; //constant
+            public float y; //changing
+            public int restingHeight; //constant
+            public float speed;
+
+            public WaterParticle(int _x, int _defaultHeight)
+            {
+                x = _x;
+                restingHeight = _defaultHeight;
+
+                y = restingHeight;
+
+                speed = 0;
+            }
+
+            public void Update(float tension, float dampening)
+            {
+                float x = restingHeight - y;
+                speed += tension * x - speed * dampening;
+                y += speed;
+            }
+        }
+
+        public BasicEffect effect;
+
+        public Vector2 ToPrimCoordinates(Vector2 point) => new Vector2(
+            (point.X - (EngineGame.instance.windowWidth / 2)) / (EngineGame.instance.windowWidth / 2),
+            (point.Y - (EngineGame.instance.windowHeight / 2)) / (EngineGame.instance.windowHeight / 2));
 
         public override void Draw(SpriteBatch spriteBatch)
         {
-            VertexPositionColorTexture[] vertices = new VertexPositionColorTexture[3];
-
-            vertices[0] = new VertexPositionColorTexture(new Vector3(20, 20, 0), Color.Blue, Vector2.Zero);
-            EngineGame.instance.GraphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleList, vertices, 0, 1);
-
             base.Draw(spriteBatch);
+
+            for (int i = 0; i < waterParticles.Count; i++)
+            {
+                WaterParticle p = waterParticles[i];
+
+                p.Update(0.025f, 0.025f);
+
+                spriteBatch.Draw(EngineHelpers.GetTexture("MagicPixel"), new Vector2(p.x, p.y) - myStage.screenPosition, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+
+                waterParticles[i] = p;
+            }
+
+            effect.CurrentTechnique.Passes[0].Apply();
+
+            for (int i = 0; i < waterParticles.Count; i++)
+            {
+                if (i < waterParticles.Count - 1)
+                {
+                    WaterParticle p = waterParticles[i];
+
+                    VertexPositionColor[] vertices = new VertexPositionColor[3];
+
+                    Vector2 thisParticle = ToPrimCoordinates(new Vector2(p.x, p.y));
+                    Vector2 nextParticle = ToPrimCoordinates(new Vector2(waterParticles[i + 1].x, waterParticles[i + 1].y));
+
+                    Vector2 belowThisParticle = ToPrimCoordinates(new Vector2(p.x, p.restingHeight - depth));
+                    Vector2 belowNextParticle = ToPrimCoordinates(new Vector2(waterParticles[i + 1].x, waterParticles[i + 1].restingHeight - depth));
+
+
+                    //Triangle between both surface points and the bottom point below this particle
+
+                    vertices[0] = new VertexPositionColor(new Vector3(thisParticle.X, thisParticle.Y, 0), Color.LightCyan);
+                    vertices[1] = new VertexPositionColor(new Vector3(nextParticle.X, nextParticle.Y, 0), Color.LightCyan);
+                    vertices[2] = new VertexPositionColor(new Vector3(belowThisParticle.X, belowThisParticle.Y, 0), Color.DarkCyan);
+
+                    EngineGame.instance.GraphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleList, vertices, 0, 1);
+
+
+                    //Triangle between both bottom points and the surface point of the next particle
+
+                    vertices[0] = new VertexPositionColor(new Vector3(belowThisParticle.X, belowThisParticle.Y, 0), Color.DarkCyan);
+                    vertices[1] = new VertexPositionColor(new Vector3(belowNextParticle.X, belowNextParticle.Y, 0), Color.DarkCyan);
+                    vertices[2] = new VertexPositionColor(new Vector3(nextParticle.X, nextParticle.Y, 0), Color.LightCyan);
+
+                    EngineGame.instance.GraphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleList, vertices, 0, 1);
+                }
+            }
+
+            float[] leftDeltas = new float[waterParticles.Count];
+            float[] rightDeltas = new float[waterParticles.Count];
+
+            float spread = 0.25f;
+
+            // do some passes where springs pull on their neighbours
+            for (int j = 0; j < 4; j++)
+            {
+                for (int i = 0; i < waterParticles.Count; i++)
+                {
+                    if (i > 0)
+                    {
+                        WaterParticle pBehind = waterParticles[i - 1];
+
+                        leftDeltas[i] = spread * (waterParticles[i].y - pBehind.y);
+                        pBehind.speed += leftDeltas[i];
+
+                        waterParticles[i - 1] = pBehind;
+                    }
+                    if (i < waterParticles.Count - 1)
+                    {
+                        WaterParticle pNext = waterParticles[i + 1];
+
+                        rightDeltas[i] = spread * (waterParticles[i].y - pNext.y);
+                        pNext.speed += rightDeltas[i];
+
+                        waterParticles[i + 1] = pNext;
+                    }
+                }
+
+                for (int i = 0; i < waterParticles.Count; i++)
+                {
+                    if (i > 0)
+                    {
+                        WaterParticle pBehind = waterParticles[i - 1];
+
+                        pBehind.y += leftDeltas[i];
+
+                        waterParticles[i - 1] = pBehind;
+                    }
+                    if (i < waterParticles.Count - 1)
+                    {
+                        WaterParticle pNext = waterParticles[i + 1];
+
+                        pNext.y += rightDeltas[i];
+
+                        waterParticles[i + 1] = pNext;
+                    }
+                }
+            }
+
+            //water code largely from https://gamedevelopment.tutsplus.com/tutorials/make-a-splash-with-dynamic-2d-water-effects--gamedev-236
+        }
+
+        public void Splash(float xPosition, float speed)
+        {
+            int index = (int)MathHelper.Clamp(xPosition / 4, 0, waterParticles.Count - 1);
+
+            for (int i = Math.Max(0, index - 0); i < Math.Min(waterParticles.Count - 1, index + 1); i++)
+            {
+                WaterParticle bingus = waterParticles[index];
+
+                bingus.speed = speed;
+
+                waterParticles[index] = bingus;
+            }
         }
     }
 }
